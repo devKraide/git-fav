85a44a9125104439e3cecc2fd335bd0d582880eb
Nicolas <nicolas.kraide@gamil.com>
2024-09-25 05:46:07 -0300

    main uptade

---
+++ js/Favoritos.js
@@ -0,0 +1,170 @@
+
+import { GithubUser } from "./GithubUser.js";
+
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root);
+    this.load();
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
+  }
+
+  save() {
+    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries));
+  }
+
+  async add(username) {
+    try {
+      const userExists = this.entries.find((entry) => entry.login === username);
+      if (userExists) {
+        throw new Error("Usuário já cadastrado");
+      }
+
+      const user = await GithubUser.search(username);
+      if (user.login === undefined) {
+        throw new Error("Usuário não encontrado!");
+      }
+
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
+
+      // Removendo a linha "Nenhum favorito ainda" se estiver presente
+      const noUserRow = this.tbody.querySelector(".tr-no-user");
+      if (noUserRow) {
+        noUserRow.remove();
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(
+      (entry) => entry.login !== user.login
+    );
+    this.entries = filteredEntries;
+    this.update();
+    this.save();
+
+    
+  }
+
+
+}
+
+// classe que vai criar a visualização e eventos do HTML
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root);
+    this.tbody = this.root.querySelector("table tbody");
+    this.update();
+    this.onadd();
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector(".search button");
+    const input = document.querySelector('input');
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector(".search input");
+      this.add(value);
+      input.value = '';
+    };
+  }
+
+  updateTbodyHeight() {
+    const rowCount = this.entries.length;
+    const rowHeight = 120; 
+    const newHeight = rowCount * rowHeight;
+    
+    if (this.entries.length === 0) {
+      this.tbody.style.height = `${rowHeight * 3.33}px`;
+    } else {
+      this.tbody.style.height = `${newHeight}px`;
+    }
+  }
+
+  update() {
+    this.removeAllTr();
+
+    if (this.entries.length === 0) {
+      this.addNoUserRow();
+    }
+
+    this.entries.forEach((user) => {
+      const row = this.createRow();
+
+      row.querySelector(
+        ".user img"
+      ).src = `https://github.com/${user.login}.png`;
+      row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
+      row.querySelector(".user p").textContent = user.name;
+      row.querySelector(".user span").textContent = user.login;
+      row.querySelector(".repositories").textContent = user.public_repos;
+      row.querySelector(".followers").textContent = user.followers;
+
+      row.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Tem certeza que deseja deletar essa linha?");
+
+        if (isOk) {
+          this.delete(user);
+        }
+      };
+
+      this.tbody.append(row);
+    });
+
+    this.updateTbodyHeight();
+  }
+
+  createRow() {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+        <td class="user">
+           <img src="https://github.com/Alexandre SR.png" alt="Imagem de maykbrito">
+           <a href="https://github.com/Alexandre SR" target="_blank">
+             <p>Alexandre SR</p>
+             <span>Alexandre Soares Rodrigues</span>
+           </a>
+         </td>
+         <td class="repositories">
+           15
+         </td>
+         <td class="followers">
+           0
+         </td>
+         <td>
+           <button class="remove">Remover</button>
+         </td>
+        `;
+
+    return tr;
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove();
+    });
+
+    this.save()
+  }
+
+  addNoUserRow() {
+    const tbody = this.root.querySelector("tbody");
+    const noUserRow = document.createElement("tr");
+    noUserRow.className = "tr-no-user";
+    noUserRow.innerHTML = `
+            <td colspan="4">
+                <div class="no-user">
+                    <img src="/assets/Estrela.svg" alt="ícone de uma estrela com uma expressão de surpresa">
+                    <span>Nenhum favorito ainda</span>
+                </div>
+            </td>
+        `;
+    tbody.appendChild(noUserRow);
+  }
+}
